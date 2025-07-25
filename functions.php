f55fe47634399de39f7e3368230e7a7ea3c2097b
Irbaz-Patel <irbazahmed1711@gmail.com>
2025-07-25 10:33:59 +0530

    updateFunctions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 function load_theme_assets() {
+      // Load custom style.css from root
+    wp_enqueue_style(
+        'main-style',
+        get_template_directory_uri() . '/style.css',
+        array(),
+        filemtime(get_template_directory() . '/style.css'),
+        'all'
+    );
     // JS file
     wp_enqueue_script('main-js', get_template_directory_uri() . '/assets/js/script.js', array(), false, true);
 }
